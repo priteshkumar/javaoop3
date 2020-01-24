a27188c4ba2726f8aac9f6f1738a38e5d2e5f398
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-01-24 17:14:03 +0530

    add checkedexception code

--- src/com/upgrad/oop3/CustomExceptionDemo.java
+++ src/com/upgrad/oop3/CustomExceptionDemo.java
@@ -1,8 +1,11 @@
 package com.upgrad.oop3;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.*;
+import java.io.File;
 
-class InvalidNameException extends Exception{
+class InvalidNameException extends RuntimeException{
     private String message;
     public InvalidNameException(String s){
         this.message = s;
@@ -23,6 +26,16 @@ public class CustomExceptionDemo {
         students.add("casper1234");
         students.add("coder-moon");
 
+        try {
+            testExit();
+        }
+        catch (IOException e) { //must to specify the catch block as called function throws IOException
+            e.printStackTrace();
+        }
+        catch(ArithmeticException e){
+            e.printStackTrace();
+        }
+
         try{
             for(String name:students){
                 verifyStudentName(name);
@@ -35,11 +48,44 @@ public class CustomExceptionDemo {
             System.out.println("student name verification completed..");
         }
 
+        try {
+            fun(50);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
     }
 
-    public static void verifyStudentName(String name) throws InvalidNameException{
+    public static void verifyStudentName(String name) {
         if(Pattern.matches(".*\\d",name) == true){
             throw new InvalidNameException("Invalid student name. name has numeric digits..");
+        }
+    }
+
+    static void fun(int n) {
+        if (n < 60)
+            throw new ArithmeticException("Not Eligible");
+        else
+            System.out.println("Eligible for pension");
+    }
+
+    static void testExit() throws IOException {
+        try {
+            File f = new File("/home/mavixk/file1");
+            Scanner input = new Scanner(f);
+            int count = 0;
+            while(input.hasNextInt()){
+                System.out.println(input.nextInt());
+                count++;
+                if (count == 1)break;
+            }
+            input.close();
+            System.out.print(50 / 0);
+            System.exit(0);
+        }
+        finally {
+            System.out.println("code inside finally block");
         }
     }
 }
